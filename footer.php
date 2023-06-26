eb939b935fef60f762fa76026c7bc5d372c8c2c0
Uriel Morales <uriel@gmail.com>
2023-06-26 13:32:05 -0600

    se agrego complementos pagina inicio

---
+++ footer.php
@@ -0,0 +1,25 @@
+<footer class="main-footer ">
+        <div class="container container--flex">
+            
+            <div class="column column--33">
+               <h2 class="column__title">Contactanos</h2>
+               <p class="column__txt" ><i class="fa-solid fa-location-dot"></i> Aeropuerto Internacional Felipe Ángeles (AIFA) – Tecámac Estado de México </p> 
+               <p class="column__txt"><i class="fa-solid fa-phone"></i> Telefono: (+52) 951 109 8256 </p> 
+               <p class="column__txt"><i class="fa-sharp fa-solid fa-envelope"></i> E-mail: rf8income@gmail.com</p> 
+            </div>
+            <div class="column column--33 ocul">
+                
+            </div>
+            <div class="column column--33">
+                <h2 class="column__title">Siguenos en nuestras redes Sociales</h2>
+                
+                <div class="social-icon">
+                    <a href="https://www.facebook.com/sinteredst" target="_blank" class="social-icon__link facebook"><span class="fa fa-facebook"></span></a>
+                    <a href="https://www.instagram.com/sinteredstone_/" target="_blank" class="social-icon__link"><img src="img/ig.png" class="ig" alt=""></a>
+                    <!-- <a href="" class="social-icon__link"><span class="icon-mail"></span></a> -->
+                </div>
+            </div>
+            <p class="copy">©2023 INCOME – Insumos Comerciales Especializados SA de CV</p>
+        </div>
+
+</footer>
